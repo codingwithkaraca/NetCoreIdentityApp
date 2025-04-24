e5e989228d195c555beb1419831bc247f82bee3e
codingwithkaraca <karacaalper806@gmail.com>
2025-04-24 20:31:52 +0300

    üyelik sayfasının tasarlanması 1

---
+++ Entities/ViewModels/UserVM.cs
@@ -0,0 +1,8 @@
+namespace Entities.ViewModels;
+
+public class UserVM
+{
+    public string? UserName { get; set; }
+    public string? Email { get; set; } 
+    public string? PhoneNumber { get; set; }
+}
--- NetCoreIdentityApp/Controllers/MemberController.cs
+++ NetCoreIdentityApp/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
 using Entities.Concrete;
+using Entities.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,14 +10,25 @@ namespace NetCoreIdentityApp.Controllers
     public class MemberController : Controller
     {
         private readonly SignInManager<User> _signInManager;
-        public MemberController(SignInManager<User> signInManager)
+        private readonly UserManager<User> _userManager;
+        public MemberController(SignInManager<User> signInManager, UserManager<User> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.FindByNameAsync(User.Identity!.Name!);
+
+            var userViewModel = new UserVM
+            {
+                UserName = currentUser!.UserName,
+                Email = currentUser!.Email,
+                PhoneNumber = currentUser!.PhoneNumber,
+            };
+            
+            return View(userViewModel);
         }
 
         // bu çıkış yap butonu çıkış yaptıktan sonra bana hangi url'e gitme avantajını sağlıyor
